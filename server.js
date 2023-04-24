69879c95e1899053c3949391845a0634ede2f7c5
zabokaa <>
2023-04-24 16:18:48 +0200

    Updated head tag

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const http = require("http");
 http.createServer((request, response) => {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello Node!\n");
-}).listen(8080);
+}).listen(5500);
 
-console.log("My first Node test server is running on Port 8080.");
+console.log("My first Node test server is running on Port 5500.");
