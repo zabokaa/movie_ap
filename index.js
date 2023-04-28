2f51f565c57c769c6b59841d63db20f8b0f36443
zabokaa <>
2023-04-28 18:01:30 +0200

    Added GET: list of all movies

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-// first: acivating nodemon in zsh: `npm run dev` !!
-
-// const app = require("express")();
-// the combined one not working when I want to use express.static .. arrg
-
 const express = require("express"),
   morgan = require("morgan"),
   fs = require("fs"),
   uuid = require("uuid"),
+  bodyParser = require("body-parser"),
   path = require("path");
+  
 const app = express();
 // //will be used for routing requests a responses
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {flags: "a"});
 
 // // middleware
 app.use(bodyParser.json());
-app.use(morgan("combined", {stream: accessLogStream}));
-app.use(express.static("public"));
+// app.use(morgan("combined", {stream: accessLogStream}));
+// app.use(express.static("public"));
 
 
 let movies = [
     {
         title: "Portrait of a Lady on Fire",
-        description: "Set in France in the late 18th century, the film tells the story of an affair between an aristocrat and a painter commissioned to paint her portrait." 
+        description: "Set in France in the late 18th century, the film tells the story of an affair between an aristocrat and a painter commissioned to paint her portrait.",
         director: {
           name: "Céline Sciamma",
           bio: "Sciamma was born on 12 November 1978 and raised in Cergy-Pontoise, a suburb outside of Paris. Before attending La Fémis, the première French film school, where she studied from 2001-2005, Sciamma earned her master's degree in French Literature at Paris Nanterre University",
@@ -32,100 +29,100 @@ let movies = [
     },
     {
         title: "Madalena",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin.",
         director: {
           name: "Madiano Marcheti",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "female"
-        }
+        },
         genre: "drama",
         year: 2021
     },
     {
         title: "Mouthpiece",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin.",
         director: {
           name: "Patricia Rozema",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "female"
-        }
+        },
         genre: "drama",
         year: 2017
     }, 
     {
         title: "Circumstance",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         director: {
           name: "Maryam Keshavarz",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "female"
-        }
+        },
         genre: "drama",
         year: 2020
     },
     {
         title: "Rafiki",
-        description: "Despite the political rivalry between their families, Kena and Ziki resist and remain close friends, supporting each other to pursue their dreams in a conservative society. When love blossoms between them, the two girls will be forced to choose between happiness and safety."
+        description: "Despite the political rivalry between their families, Kena and Ziki resist and remain close friends, supporting each other to pursue their dreams in a conservative society. When love blossoms between them, the two girls will be forced to choose between happiness and safety.",
         director: {
           name: "Wanuri Kahiu",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "female"
-        }
+        },
         genre: "drama",
         year: 2018
     },
     {
         title: "Amour Fou",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         director: {
           name: "Jessica Hausner",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "female"
-        }
+        },
         genre: "drama",
         year: 2021
     },
     {
         title: "Ghosts",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         director: {
           name: "Christian Petzold",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "male"
-        }
+        },
         genre: "drama",
         year: 2005
     },
     {
         title: "Why Does Herr R. Run Amok?",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         director: {
           name: "Rainer Werner Fassbinder",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "male"
-        }
+        },
         genre: "drama",
         year: 1980
     },
     {
         title: "Titane",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         director: {
           name: "Julia Ducournau",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "female"
-        }
+        },
         genre: "thriller",
         year: 2020
     },
     {
         title: "20,000 Species of Bees",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         director: {
           name: "Estibaliz Urresola Solaguren",
           bio: "Duis tristique sollicitudin nibh sit amet. Interdum posuere lorem ipsum dolor sit amet consectetur.",
           gender: "female"
-        }
+        },
         genre: "drama",
         year: 2023
     }
@@ -150,7 +147,6 @@ let movies = [
   ]
   
 //   app.METHOD(PATH, HANDLER)
-// URL and sub-URLs endpoints: /users AND /users/[ID] AND /users[ID]/fav etc.
 
   // POST (create)
 
@@ -161,7 +157,7 @@ let movies = [
   });
   
   app.get("/movies", (req, res) => {
-    res.json(movies);
+    res.status(200).json(movies);
   });
 
 
@@ -169,11 +165,12 @@ let movies = [
 
   // PUT (update)
 
+
   // DELETE
 
 
 // error handling middleware - always last but before listen
-  const bodyParser = require("body-parser"),
+  
     methodOverride = require("method-override");
 
   app.use(bodyParser.urlencoded({
