3535fdcfeeca7c4e2d1091602ac43ec16135f128
zabokaa <>
2023-05-11 11:15:38 +0200

    Configured schema + models w/ mongoose

---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoos.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    genre: {
+        name: String,
+        description: String
+    },
+    director: {
+        name: String,
+        bio: String,
+        birthyear: Date,
+        gender: String
+    },
+    actors: [String],
+    imagePath: String,
+    featured: Boolean,
+    year: Date
+});
+
+let userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: mongoose.Mixed, required: true},
+    bday: Date,
+    favMovies: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
+});
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
