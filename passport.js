4453ff44fee8b813db0b21d82889d69f7bca869f
zabokaa <>
2023-05-18 12:59:17 +0200

    Installed + configured Passport(strategies)

---
+++ passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Models = require("./models.js");
+const passportJWT = require("passport-jwt");
+
+const Users = Models.Users;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJWT;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    (username, password, callback) => {
+      console.log(username + " " + password);
+      Users.findOne({ username: username }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+
+        if (!user) {
+          console.log("username incorrect");
+          return callback(null, false, {
+            message: "username or password are not correct :/",
+          });
+        }
+
+        console.log("finished");
+        return callback(null, user);
+      });
+    }
+  )
+);
