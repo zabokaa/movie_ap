6a90593f02fb091b2bfd122b374395cd5fd9169e
zabokaa <>
2023-05-19 15:54:35 +0200

    Edited passport.js

--- passport.js
+++ passport.js
@@ -13,46 +13,42 @@ passport.use(
       usernameField: "username",
       passwordField: "password",
     },
-    (username, password) => {
+    (username, password, callback) => {
       console.log(username + " " + password);
-      
-      return Users.findOne({ username: username })
+
+      Users.findOne({ username: username })
         .then((user) => {
           if (!user) {
             console.log("username incorrect");
-            return Promise.reject({
+            return callback(null, false, {
               message: "username or password are not correct :/",
             });
           }
 
           console.log("finished");
-          return Promise.resolve(user);
-        })
-        .catch((error) => {
+          return callback(null, user);
+        }).catch((error) => {
           console.log(error);
-          return Promise.reject(error);
-        });
+          return callback(error);
+        })
     }
   )
 );
 
-
 passport.use(
-    new JWTStrategy(
-      {
-        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("Bearer"),
-        secretOrKey: "your_jwt_secreta",
-      },
-      (jwtPayload, callback) => {
-        return Users.findById(jwtPayload._id)
-          .then((user) => {
-            return callback(null, user);
-          })
-          .catch((error) => {
-            return callback(error);
-          });
-      }
-    )
-  );
-  
-  
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("Bearer"),
+      secretOrKey: "your_jwt_secreta",
+    },
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
