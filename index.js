3c48e9f4c0b1fa751bea1731a68352869915f7e3
zabokaa <>
2023-06-14 12:38:22 +0200

    Added comments

--- index.js
+++ index.js
@@ -20,17 +20,18 @@ const Users = Models.User;
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
 // })
+// .then(() => {
+//   console.log("Connected to MongoDB");
+// })
+// .catch((error) => {
+//   console.error("Error connecting to MongoDB", error);
+// });
+
+// CONNECTING to mongodbAtlas w/ ENV VAR:
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
-.then(() => {
-  console.log("Connected to MongoDB");
-})
-.catch((error) => {
-  console.error("Error connecting to MongoDB", error);
 });
-
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
