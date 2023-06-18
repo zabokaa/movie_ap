188a7add52d28a4a7ac27018288610e418aa63de
zabokaa <>
2023-06-18 18:00:51 +0200

    Added jwt passport 4 list of users

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.get("/movies/director/:director/movies", passport.authenticate("jwt", {sessi
     });
 });
 // list of all users:
-app.get("/users", (req,res) => {
+app.get("/users", passport.authenticate("jwt", {session: false}), (req,res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
